9e9866e0cdcd3b0edd76ea032fc96169fe1ab57b
Declan Bernardin <dmjbernardin@yahoo.com>
2020-02-26 20:16:46 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+
+  fizzBuzz = () => {
+    let numbers = [1]
+    let one = 1 
+    for ( let i = 0; i < numbers.length; i++){
+      if (numbers[i] < 100 ){
+        one +=1
+        numbers.push(one)
+      }
+      console.log(numbers)
+    }
+    for (let i = 0; i< numbers.length; i++){
+      if (numbers[i] % 3 === 0 && numbers[i] % 5 === 0){
+        console.log("HomeSpotter")
+      }
+      else if (numbers[i] % 3 === 0){
+        console.log("Home")
+      }
+      else if (numbers[i] % 5 === 0){
+        console.log("Spotter")
+      } else {
+        console.log("this number is not divisible", numbers[i])
+      }
+    
+  }
+  }
+
+
+
+
+  render() {
+    return(
+      <div style={{ textAlign: 'center' }}>
+        <h1 >
+    Function FizzBuzz  
+        </h1>
+      <button onClick={this.fizzBuzz}>Activate FizzBuzz</button>
+      </div>
+    )
+  }
 }
 
 export default App;
