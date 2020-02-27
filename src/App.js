05711a51bdfee41eac82dcd21d867670afe48598
Declan Bernardin <dmjbernardin@yahoo.com>
2020-02-26 21:11:27 -0600

    displayed on the dom

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 
 class App extends Component {
 
-
+  state = {
+    outcome: []
+  }
+ 
   HomeSpotter = () => {
     let numbers = [1]
     let one = 1 
@@ -17,29 +20,47 @@ class App extends Component {
     for (let i = 0; i< numbers.length; i++){
       if (numbers[i] % 3 === 0 && numbers[i] % 5 === 0){
         console.log("HomeSpotter")
+        this.setState({
+          ...this.state.outcome.push("HomeSpotter")
+        })
       }
       else if (numbers[i] % 3 === 0){
         console.log("Home")
+        this.setState({
+          ...this.state.outcome.push("Home")
+        })
       }
       else if (numbers[i] % 5 === 0){
         console.log("Spotter")
+        this.setState({
+          ...this.state.outcome.push("Spotter")
+        })
       } else {
         console.log("this number is not divisible", numbers[i])
+        this.setState({
+          ...this.state.outcome.push(numbers[i])
+        })
       }
     
   }
   }
 
-
-
-
   render() {
+
+    let fizzBuzz = this.state.outcome.map((outcome) => {
+      return( 
+        <div>
+          <p>{outcome}</p>
+        </div>
+      )
+    })
     return(
       <div style={{ textAlign: 'center' }}>
         <h1 >
         Function HomeSpotter 
         </h1>
       <button onClick={this.HomeSpotter}>Activate HomeSpotter</button>
+      {fizzBuzz}
       </div>
     )
   }
